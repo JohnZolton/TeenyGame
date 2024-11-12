c4ce552765e0900db1df4979f810b7b3003e4db4
John <jgz6@uakron.edu>
2024-11-11 20:18:29 -0500

    pusher auth fix

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from "next/server";
+import { pusher } from "~/lib/pusher";
+
+export async function POST(request: NextRequest) {
+  try {
+    const formData = await request.formData();
+    const socketId = formData.get("socket_id");
+    const channelName = formData.get("channel_name");
+
+    if (!(socketId ?? channelName)) {
+      throw new Error("Required field socket_id is missing");
+    }
+    if (typeof socketId !== "string" || typeof channelName !== "string") {
+      throw new Error(
+        "Required fields socket_id and/or channel_name are missing or invalid.",
+      );
+    }
+
+    // dev dummy data, replace with nostr stuff
+    const user = {
+      id: `user_${Math.random().toString(36).substr(2, 9)}`,
+      user_info: {
+        name: `user_${Math.random().toString(36).substr(2, 9)}`,
+      },
+    };
+
+    // Authenticate the user, dev => everyone gets approved
+    const authResponse = pusher.authorizeChannel(
+      socketId.toString(),
+      channelName.toString(),
+      {
+        user_id: user.id,
+        user_info: user.user_info,
+      },
+    );
+
+    return NextResponse.json(authResponse);
+  } catch (error) {
+    console.error("Error authenticating with Pusher:", error);
+    return NextResponse.json(
+      { error: "Authentication failed" },
+      { status: 403 },
+    );
+  }
+}
--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -12,6 +12,7 @@ export const pusher = new Pusher({
 export const pusherClient = new PusherClient(
   process.env.NEXT_PUBLIC_PUSHER_KEY!,
   {
+    authEndpoint: "/api/pusher/auth",
     cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
   },
 );
